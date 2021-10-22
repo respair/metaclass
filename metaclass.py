27bd9e5f6a2a322141d04be418d6e684592b4580
Respair <senselessly@list.ru>
2021-10-22 18:40:41 +0300

    added comments

--- metaclass.py
+++ metaclass.py
@@ -1,7 +1,11 @@
+"""metaclass"""
+
 class MakeCustomMetaclass(type):
+    """a metaclass that changes the name of attributes (except for special ones)"""
 
     def __new__(mcs, class_name,
                 class_parents, class_attr):
+        """adding custom_ to the beginning of the name"""
 
         case_attr = {}
         for name, val in class_attr.items():
@@ -15,10 +19,13 @@ class MakeCustomMetaclass(type):
 
 
 class CustomClass(metaclass=MakeCustomMetaclass):
+    """used in main to validate metaclass"""
     x = 50
 
     def __init__(self, val=99):
+        """constructor"""
         self.val = val
 
     def line(self):
+        """just function"""
         return 100
--- newlist.py
+++ newlist.py
@@ -1,22 +1,30 @@
+"""list"""
+
 class NewList(list):
+    """list with advanced features"""
 
     def __init__(self, arr=None):
+        """constructor"""
         if arr is None:
             arr = []
         self.list = arr
         self.len = len(arr)
 
     def append(self, item):
+        """adding an element to the end"""
         self.list.append(item)
         self.len = len(self.list)
 
     def copy(self, arr):
+        """copy one array to another"""
         self.list = []
         for i in range(len(arr)):
             self.list.append(arr[i])
         self.len = len(self.list)
 
     def new_size_list(self, other):
+        """creating a list based on the original one
+        with the addition of zeros"""
         list_time = self.list[:]
         if self.len < len(other):
             i = self.len
@@ -26,9 +34,11 @@ class NewList(list):
         return NewList(list_time)
 
     def __len__(self):
+        """overriding the method to get the length of the list"""
         return self.len
 
     def __add__(self, other):
+        """redefining the + operator"""
         list_time = self.new_size_list(other)
         for j in range(len(other)):
             list_time.list[j] = list_time.list[j] + other[j]
@@ -37,12 +47,14 @@ class NewList(list):
     __radd__ = __add__
 
     def __sub__(self, other):
+        """redefining the - operator"""
         list_time = self.new_size_list(other)
         for j in range(len(other)):
             list_time.list[j] = list_time.list[j] - other[j]
         return list_time
 
     def inverse_sub(self, other):
+        """redefining the + operator for reverse order"""
         list_time1 = other[:]
         if self.len > len(other):
             i = len(other)
@@ -57,22 +69,28 @@ class NewList(list):
     __rsub__ = inverse_sub
 
     def __getitem__(self, key):
+        """override get item"""
         return self.list[key]
 
     def sum_of_list(self, arr):
+        """getting the sum of the elements of a list"""
         total = 0
         for i in range(len(arr)):
             total = total + arr[i]
         return total
 
     def __lt__(self, other):
+        """overriding operator <"""
         return self.sum_of_list(self.list) < self.sum_of_list(other)
 
     def __eq__(self, other):
+        """overriding operator =="""
         return self.sum_of_list(self.list) == self.sum_of_list(other)
 
     def __ne__(self, other):
+        """overriding operator !="""
         return self.sum_of_list(self.list) != self.sum_of_list(other)
 
     def __gt__(self, other):
+        """overriding operator >"""
         return self.sum_of_list(self.list) > self.sum_of_list(other)
